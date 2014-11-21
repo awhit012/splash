2e02e26a20d856572b7426157df680e0ff05e07e
Alex White <awhit012@gmail.com>
2014-11-20 18:12:25 -0800

    kitchen sink commit -- a little style, users displayed at /users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,17 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require ./app
+
+// for more details see: http://emberjs.com/guides/application/
+window.App = Ember.Application.create({
+ LOG_TRANSITIONS: true,
+ LOG_TRANSITIONS_INTERNAL: true,
+ LOG_VIEW_LOOKUPS: true
+});
+
 //= require_tree .
---
+++ app/assets/javascripts/models/users.js
@@ -0,0 +1,5 @@
+App.User = DS.Model.extend({
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  email: DS.attr('string'),
+})
